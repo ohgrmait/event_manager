2d1fd2d113f72e41545fbbe951dad1297c4995c3
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2025-01-02 16:38:37 +0530

    Iteration 3: Clean legislator names in a zip code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -26,7 +26,7 @@ contents.each do |row|
       levels: 'country',
       roles: %w[legislatorUpperBody legislatorLowerBody]
     )
-    legislators = legislators.officials
+    legislators = legislators.officials.map(&:name).join(', ')
   rescue StandardError
     legislators = 'You can find your representatives by visiting ' \
                   'www.commoncause.org/take-action/find-elected-officials'
