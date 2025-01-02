30f447a1204281af74318484e6f58f91f0471b29
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2025-01-02 11:54:21 +0530

    Iteration 1: Parse with CSV and display zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,15 @@
+require 'csv'
+
 puts 'Event Manager Initialized!'
 
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index.zero?
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
 
-  row = line.split(',')
-  name = row[2]
-  puts name
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name}, #{zipcode}"
 end
