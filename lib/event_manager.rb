3e28b835459d4628d58246efc83fecf739ea5549
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2025-01-02 11:38:29 +0530

    Iteration 0: Display first names of all attendees

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,10 @@
 puts 'Event Manager Initialized!'
+
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line, index|
+  next if index.zero?
+
+  row = line.split(',')
+  name = row[2]
+  puts name
+end
