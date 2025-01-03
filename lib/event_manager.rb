97110d92450e0b19003f2b5ab1a8562bb8e5e006
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2025-01-03 22:32:41 +0530

    Assignment: Get peak hours and most days of in week

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -44,6 +44,28 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+timings = {}
+
+def peak_registration_hours(timings, reg_time)
+  time = Time.strptime(reg_time, '%k:%M')
+  if timings.key?(time.hour)
+    timings[time.hour] += 1
+  else
+    timings[time.hour] = 1
+  end
+end
+
+registrations = {}
+
+def most_people_registered(registrations, reg_date)
+  date = Date.strptime(reg_date, '%m/%d/%Y')
+  if registrations.key?(date.strftime('%A'))
+    registrations[date.strftime('%A')] += 1
+  else
+    registrations[date.strftime('%A')] = 1
+  end
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
@@ -57,10 +79,27 @@ erb_template = ERB.new template_letter
 
 contents.each do |row|
   id = row[0]
+
   name = row[:first_name]
+
   phone = clean_homephone(row[:homephone])
+
   zipcode = clean_zipcode(row[:zipcode])
+
   legislators = legislators_by_zipcode(zipcode)
+
+  reg_time = row[:regdate].split[1]
+  reg_date = row[:regdate].split[0]
+  peak_registration_hours(timings, reg_time)
+  most_people_registered(registrations, reg_date)
+
   form_letter = erb_template.result(binding)
+
   save_thank_you_letter(id, form_letter)
 end
+
+peak_hours = timings.select { |time, count| count == timings.values.max }
+puts "Peak Registration Hours = #{peak_hours.keys.join(', ')}"
+
+most_days = registrations.select { |registration, count| count == registrations.values.max }
+puts "Most Days of the Week Registered = #{most_days.keys.join(', ')}"
